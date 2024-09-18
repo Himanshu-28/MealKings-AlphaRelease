16da93cdeee11c8fa1859a8b6ff48985a554342a
GayatriGalav <137135841+GayatriGalav@users.noreply.github.com>
2024-09-19 01:53:39 +0530

    Delete authentication-service directory

--- authentication-service/src/main/java/com/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import java.util.Optional;
-import com.entity.User;
-
-public interface UserService {
-    User registerUser(User user);
-    Optional<User> loginUser(String email, String password);
-}
