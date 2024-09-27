7cac389a78ebc531c8f3ef46ae6922b9560f5ab8
Tharun2249 <76117512+Tharun2249@users.noreply.github.com>
2024-09-27 19:27:59 +0530

    Delete order-place-app directory

--- order-place-app/src/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import orderReducer from './orderSlice';
-
-export const store = configureStore({
-  reducer: {
-    order: orderReducer,
-  },
-});
